e452b3bb1fd49a47b5d65b06a404ba325126c87e
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-03-29 10:08:10 +0800

    Update web.php
    add group controller for AdminController and rename url register-user to PostUser

--- routes/web.php
+++ routes/web.php
@@ -7,31 +7,39 @@ use Inertia\Inertia;
 use App\Http\Controllers\AdminController;
 
 
-Route::post('/PostLogin', [AdminController::class, 'PostLogin'])->name('post.login');
+Route::get('/', function () {
+    return redirect('/login');
+});
 
-Route::post('/register-user', [AdminController::class, 'RegisterUser']);
+   
+Route::post('/PostLogin', [AdminController::class, 'PostLogin'])->name('post.login');
+Route::post('/PostUser', [AdminController::class, 'RegisterUser']);
 
 Route::middleware(['admin'])->group(function () {
 
-    Route::inertia('/login', 'Auth/Login');
-
-    Route::inertia('/register', 'Auth/Register');
-
-    Route::get('/dashboard', [AdminController::class, 'Dashboard']);
-     
-    Route::get('/products', [AdminController::class, 'Products']);
+    Route::inertia('/login', 'Auth/Login')->name('/login');
     
+    Route::inertia('/register', 'Auth/Register')->name('/register');
+
     Route::inertia('/create-product', 'CreateProduct');
-    Route::post('/post-product', [AdminController::class, 'PostProduct']);
-
-    Route::delete('/delete-product/{id}', [AdminController::class, 'DeleteProduct']);
-
-    Route::put('/update-product', [AdminController::class, 'UpdateProduct']);
 
     Route::inertia('/videos', 'Videos');
 
-    Route::get('/videos/{title}', [AdminController::class, 'VideoPlayer']);
+    Route::controller(AdminController::class)->group( function () {
+        Route::get('/dashboard', 'Dashboard');
 
-    Route::get('/logout', [AdminController::class, 'logout']); 
+        Route::get('/products', 'Products');
+
+        Route::post('/post-product','PostProduct');
+
+        Route::delete('/delete-product/{id}', 'DeleteProduct');
+
+        Route::put('/update-product', 'UpdateProduct');
+
+        Route::get('/videos/{title}', 'VideoPlayer');
+
+        Route::get('/logout', 'logout'); 
+    });
+    
 });
 
