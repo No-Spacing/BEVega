c0e93dbdaf92cb64b7a0d35777807a704ff4802c
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-03-29 10:08:42 +0800

    Update AdminMiddleware.php
    prevent user from going back to the last page after logout

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -24,6 +24,8 @@ class AdminMiddleware
             return redirect('/dashboard');
         }
 
-        return $next($request);
+        return $next($request)->header('Cache-Control','no-cache, no-store, max-age=0, must-revalidate')
+                ->header('Pragma','no-cache')
+                ->header('Expires','Sat 01 Jan 1990 00:00:00 GMT');
     }
 }
